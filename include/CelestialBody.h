50eb9068f8bc18c3c34616274634fa8c6dc7ea46
Riley Spires <rjs243@uakron.edu>
2025-02-20 17:44:15 -0500

    Encapsulated CelestialBody

--- include/CelestialBody.h
+++ include/CelestialBody.h
@@ -3,7 +3,7 @@
 
 class CelestialBody
 { 
-public: // TODO - Change to private and provide accessors - cont. in "SolarSystem.cpp" in function "getAttractionForce()"
+private: 
   double mass;
   double volume;
   double radius;
@@ -31,6 +31,95 @@ public:
     this->satellites = satellites;
     this->ring = ring;
   }
+
+  double getMass() const {
+    return this->mass;
+  }
+
+  double getVolume() const {
+    return this->volume;
+  }
+
+  double getRadius() const {
+    return this->radius;
+  }
+
+  double getDensity() const {
+    return this->density;
+  }
+
+  double getVelocity() const {
+    return this->velocity;
+  }
+
+  double getOrbit() const {
+    return this->orbit;
+  }
+
+  double getGravity() const {
+    return this->gravity;
+  }
+
+  double getPerihelion() const {
+    return this->perihelion;
+  }
+
+  double getAphelion() const {
+    return this->aphelion;
+  }
+
+  int getSatellites() const {
+    return this->satellites;
+  }
+
+  bool getRing() const {
+    return this->ring;
+  }
+
+  void getMass(double mass) {
+    this->mass = mass;
+  }
+
+  void getVolume(double volume) {
+    this->volume = volume;
+  }
+
+  void getRadius(double radius) {
+    this->radius = radius;
+  }
+
+  void getDensity(double density) {
+    this->density = density;
+  }
+
+  void getVelocity(double velocity) {
+    this->velocity = velocity;
+  }
+
+  void getOrbit(double orbit) {
+    this->orbit = orbit;
+  }
+
+  void getGravity(double gravity) {
+    this->gravity = gravity;
+  }
+
+  void getPerihelion(double perihelion) {
+    this->perihelion = perihelion;
+  }
+
+  void getAphelion(double aphelion) {
+    this->aphelion = aphelion;
+  }
+
+  void getSatellites(int satellites) {
+    this->satellites = satellites;
+  }
+
+  void getRing(bool ring) {
+    this->ring = ring;
+  }
+
   ~CelestialBody() { };
 };
 
