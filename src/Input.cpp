8b059b8e6659000704d7ccaea1c9d999794257a7
Cloudy <cloudisms@protonmail.com>
2025-04-25 17:51:17 -0400

    Add 1x timeScale factor

--- src/Input.cpp
+++ src/Input.cpp
@@ -8,14 +8,17 @@ void handleInput()
 {
   if (IsKeyPressed(KEY_EQUAL) && IsKeyDown(KEY_LEFT_SHIFT))
   {
-    if (Simulation::timeScale == 1000)
+    if (Simulation::timeScale == 1000 || Simulation::timeScale == 1)
       Simulation::timeScale = 10000;
     else
       Simulation::timeScale += Simulation::bigTimeScaleModifier;
   }
   else if (IsKeyPressed(KEY_EQUAL))
   {
-    Simulation::timeScale += Simulation::timeScaleModifier;
+    if (Simulation::timeScale == 1)
+      Simulation::timeScale = 1000;
+    else
+      Simulation::timeScale += Simulation::timeScaleModifier;
   }
   else if (IsKeyPressed(KEY_MINUS) && IsKeyDown(KEY_LEFT_SHIFT))
   {
@@ -25,12 +28,16 @@ void handleInput()
     {
       if (Simulation::timeScale - Simulation::bigTimeScaleModifier > 0)
         Simulation::timeScale -= Simulation::bigTimeScaleModifier;
+      else
+        Simulation::timeScale = 1;
     }
   }
   else if (IsKeyPressed(KEY_MINUS))
   {
     if (Simulation::timeScale - Simulation::timeScaleModifier > 0)
       Simulation::timeScale -= Simulation::timeScaleModifier;
+    else
+      Simulation::timeScale = 1;
   }
   else if (IsKeyPressed(KEY_R))
   {
